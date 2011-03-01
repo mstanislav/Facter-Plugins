217df0db9c0f46260ceed402a4e603430bc7816c
Mark Stanislav <mark.stanislav@gmail.com>
2011-03-01 18:02:19 -0500

    Updated to utilize many-to-one services to scripts

--- services.rb
+++ services.rb
@@ -2,25 +2,13 @@
 
 require 'facter'
 
-@facter = "/usr/bin/facter"
 @chkconfig = "/sbin/chkconfig"
 @status = "/sbin/status"
 
-exit unless File.exists?(@facter) and File.exists?(@chkconfig)
-
-if Facter.value('lsbdistid').match(/RedHatEnterpriseServer|CentOS/)
-    @distro = "RHEL"
-elsif Facter.value('lsbdistid').match(/Ubuntu|Debian/)
-    @distro = "Debian"
-end
-
-@services = {   "Debian" => {   "mysql" => "mysql", "nagios" => "nagios3", "ssh" => "ssh", "munin-node" => "munin-node", "spamassassin" => "spamassassin", "ntp" => "ntp",
-                                "bind" => "bind9", "postfix" => "postfix", "nrpe" => "nagios-nrpe-server", "apache" => "apache2", "cron" => "cron", "snmp" => "snmpd", "puppet" => "puppet",
-                                "mcollective" => "mcollective" },
-                "RHEL" => {     "mysql" => "mysqld", "nagios" => "nagios", "ssh" => "sshd", "munin-node" => "munin-node", "spamassassin" => "spamassassin", "ntp" => "ntpd",
-                                "bind" => "named", "postfix" => "postfix", "nrpe" => "nrpe", "apache" => "httpd", "cron" => "crond", "snmp" => "snmpd", "puppet" => "puppet",
-                                "mcollective" => "mcollective", "activemq" => "activemq", "postfix" => "postfix", "storman" => "stor_agent", "hobbit" => "hobbit-client", "syslog" => "syslog" },
-}
+@services = {   "mysql" => "mysql|mysqld", "nagios" => "nagios", "ssh" => "sshd", "munin-node" => "munin-node", "spamassassin" => "spamassassin", "ntp" => "ntpd", "ctasd" => "ctasd",
+                "bind" => "named", "postfix" => "postfix", "nrpe" => "nrpe", "apache" => "httpd", "cron" => "crond", "snmp" => "snmpd", "puppet" => "puppet", "ctipd" => "ctipd",
+                "mcollective" => "mcollective", "activemq" => "activemq", "postfix" => "postfix", "storman" => "stor_agent", "hobbit" => "hobbit-client", "syslog" => "syslog", "rbldns" => "rbldnsd",
+                "clamav" => "clamd", "haproxy" => "haproxy", "jboss" => "jboss_init_redhat", "monit" => "monit", "openmanage" => "dsm_om_shrsvc" }
 
 @apache_modules = [ "passenger" ]
 
@@ -32,12 +20,14 @@ def check_upstart(service)
     return %x[if [ "$(#{@status} #{service} 2> /dev/null | grep 'start/')" != "" ]; then echo 0; else echo 1; fi].chomp.to_i
 end
 
-def check_service(service)
-    if @services[@distro].has_value?(service) and (check_init(service) == 0 or check_upstart(service) == 0)
-        return 0
-    else
-        return 1
-    end
+def check_service(services)
+    services.split("|").each { |service|
+        if (check_init(service) == 0 or check_upstart(service) == 0)
+            return 0
+        end
+    }
+
+    return 1
 end
 
 def check_apache_module(mod)
@@ -46,7 +36,7 @@ end
 
 fact_services = Array.new
 
-@services[@distro].each { |service, value| fact_services << service if check_service(value) == 0 }
+@services.each { |service, value| fact_services << service if check_service(value) == 0 }
 @apache_modules.each { |mod| fact_services << mod if check_apache_module(mod) >= 1 }
 
 Facter.add("services") do
@@ -54,4 +44,3 @@ Facter.add("services") do
         fact_services.sort.join(",")
     end
 end
-
